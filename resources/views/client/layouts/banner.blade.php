9d9ab5936f112eb0fe29e32de1cd6ec666253df2
chien <chientdph38606@fpt.edu.vn>
2025-08-01 23:31:30 +0700

    cập nhật lại thống kê và banner

--- resources/views/client/layouts/banner.blade.php
+++ resources/views/client/layouts/banner.blade.php
@@ -1,24 +1,24 @@
-			<div class="hero">
-			    <div class="container">
-			        <div class="row justify-content-between">
-			            <div class="col-lg-5">
-			                <div class="intro-excerpt">
-			                    <h1>Modern Interior <span clsas="d-block">Design Studio</span></h1>
-			                    <p class="mb-4">Lấy cảm hứng từ những cảm xúc trong tâm hồn người phụ nữ – lúc dịu dàng như nốt
-			                        trầm, khi rực rỡ như nốt cao – Charming Notes là bản hòa tấu, nơi mỗi thiết kế là một thanh
-			                        âm riêng, khắc họa vẻ đẹp của từng cung bậc xúc cảm khác nhau . Tựa như một bản nhạc cổ điển
-			                        được chơi bằng tất cả sự nâng niu và tinh tế, bộ sưu tập mới nhất của dòng Senora cao cấp cất
-			                        lên những thanh âm thanh lịch, sâu lắng và đầy mê hoặc.</p>
-			                    <p><a href="{{ route('client.products.index') }}" class="btn btn-secondary me-2">Mua ngay</a><a
-			                            href="{{ route('client.about.index') }}" class="btn btn-white-outline">Khám phá</a>
-			                    </p>
-			                </div>
-			            </div>
-			            <div class="col-lg-7">
-			                <div class="hero-img-wrap">
-			                    <img src="{{ asset('assets/images/1ac70bce86c6fcf8e4d030f209bac0e1.')}}" class="img-fluid">
-			                </div>
-			            </div>
-			        </div>
-			    </div>
+<div class="hero">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-lg-5">
+				<div class="intro-excerpt">
+					<h1>Modern Interior <span clsas="d-block">Design Studio</span></h1>
+					<p class="mb-4">Lấy cảm hứng từ những cảm xúc trong tâm hồn người phụ nữ – lúc dịu dàng như nốt
+						trầm, khi rực rỡ như nốt cao – Charming Notes là bản hòa tấu, nơi mỗi thiết kế là một thanh
+						âm riêng, khắc họa vẻ đẹp của từng cung bậc xúc cảm khác nhau . Tựa như một bản nhạc cổ điển
+						được chơi bằng tất cả sự nâng niu và tinh tế, bộ sưu tập mới nhất của dòng Senora cao cấp cất
+						lên những thanh âm thanh lịch, sâu lắng và đầy mê hoặc.</p>
+					<p><a href="{{ route('client.products.index') }}" class="btn btn-secondary me-2">Mua ngay</a><a
+							href="{{ route('client.about.index') }}" class="btn btn-white-outline">Khám phá</a>
+					</p>
+				</div>
 			</div>
+			<div class="col-lg-7">
+				<div class="hero-img-wrap">
+					<img src="{{ asset('assets/images/1ac70bce86c6fcf8e4d030f209bac0e1.')}}" class="img-fluid">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
